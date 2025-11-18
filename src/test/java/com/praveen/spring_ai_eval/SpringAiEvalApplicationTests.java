d8b336d330e844819f68e9cfaa2711f9d997d639
praveenkumar09 <tpkprav@gmail.com>
2025-11-18 17:02:02 +0800

    Added example for Fact Checking Evaluator with RAG/prompt context

--- src/test/java/com/praveen/spring_ai_eval/SpringAiEvalApplicationTests.java
+++ src/test/java/com/praveen/spring_ai_eval/SpringAiEvalApplicationTests.java
@@ -7,12 +7,19 @@ import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.evaluation.FactCheckingEvaluator;
 import org.springframework.ai.chat.evaluation.RelevancyEvaluator;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.evaluation.EvaluationRequest;
 import org.springframework.ai.evaluation.EvaluationResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.util.Assert;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -37,6 +44,9 @@ class SpringAiEvalApplicationTests {
     private final float minRelevancyScore = 0.7F;
 
     private FactCheckingEvaluator factCheckingEvaluator;
+
+    @Value("classpath:/promptTemplates/hrPolicy.st")
+    Resource hrPolicyTemplate;
 
     @BeforeEach
     void setup() {
@@ -85,6 +95,33 @@ class SpringAiEvalApplicationTests {
         String response = chatController.chat(question);
 
         EvaluationRequest evaluationRequest = new EvaluationRequest(question,response);
+        EvaluationResponse evaluateResponse = factCheckingEvaluator.evaluate(evaluationRequest);
+
+        Assertions.assertAll(
+                () -> assertThat(response).isNotBlank(),
+                () -> assertThat(evaluateResponse.isPass())
+                        .withFailMessage("Answer was not factually correct for the question: " + question)
+                        .isTrue()
+        );
+
+    }
+
+    @Test
+    @DisplayName("Should return factual information for gravity question")
+    @Timeout(30)
+    void evaluateHRPolicyAnswerWithRAGContext() throws IOException {
+        //Given
+        String question = "How many paid leaves do the employees get annually?";
+
+        //When
+        String response = chatController.promptStuffing(question);
+
+        String retrievedContext = hrPolicyTemplate.getContentAsString(StandardCharsets.UTF_8);
+
+        EvaluationRequest evaluationRequest = new EvaluationRequest(
+                question,
+                List.of(new Document(retrievedContext)),
+                response);
         EvaluationResponse evaluateResponse = factCheckingEvaluator.evaluate(evaluationRequest);
 
         Assertions.assertAll(
