755c091940fc7128960caed4f8ea04df3e9fd2ed
praveenkumar09 <tpkprav@gmail.com>
2025-11-18 15:53:30 +0800

    Added example for Relevancy Checking Evaluator

--- src/test/java/com/praveen/spring_ai_eval/SpringAiEvalApplicationTests.java
+++ src/test/java/com/praveen/spring_ai_eval/SpringAiEvalApplicationTests.java
@@ -1,13 +1,71 @@
 package com.praveen.spring_ai_eval;
 
-import org.junit.jupiter.api.Test;
+import com.praveen.spring_ai_eval.controller.ChatController;
+import org.junit.jupiter.api.*;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.evaluation.RelevancyEvaluator;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.evaluation.EvaluationRequest;
+import org.springframework.ai.evaluation.EvaluationResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.util.Assert;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestPropertySource(properties = {
+        "spring.ai.openai.api-key=${OPENAI_API_KEY:test-key}",
+        "logging.level.org.springframework.ai=DEBUG"
+        })
 class SpringAiEvalApplicationTests {
 
+    @Autowired
+    private ChatController chatController;
+
+    @Autowired
+    private ChatModel chatModel;
+
+    private ChatClient chatClient;
+
+    private RelevancyEvaluator relevancyEvaluator;
+
+    private final float minRelevancyScore = 0.7F;
+
+    @BeforeEach
+    void setup() {
+        ChatClient.Builder builder = ChatClient.builder(chatModel)
+                .defaultAdvisors(new SimpleLoggerAdvisor());
+        this.chatClient = builder.build();
+        this.relevancyEvaluator = new RelevancyEvaluator(builder);
+    }
+
 	@Test
-	void contextLoads() {
-	}
+    @DisplayName("Should return relevant response for basic geography question")
+    @Timeout(30)
+	void evaluateChatControllerResponseRelevancy(){
+        //Given
+        String question = "What is the capital of India?";
+
+        //When
+        String response = chatController.chat(question);
+
+        EvaluationRequest evaluationRequest = new EvaluationRequest(question,response);
+        EvaluationResponse evaluateResponse = relevancyEvaluator.evaluate(evaluationRequest);
+
+        Assertions.assertAll(
+                () -> assertThat(response).isNotBlank(),
+                () -> assertThat(evaluateResponse.isPass())
+                .withFailMessage("Evaluation failed for question: " + question)
+                .isTrue(),
+                () -> assertThat(evaluateResponse.getScore())
+                .withFailMessage("Evaluation score for question: " + question + " is less than " + minRelevancyScore)
+                        .isGreaterThan(minRelevancyScore)
+        );
+
+    }
 
 }
