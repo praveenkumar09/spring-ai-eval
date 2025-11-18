efb9d370718717ad2046b6184d438423d23f70d2
praveenkumar09 <tpkprav@gmail.com>
2025-11-18 08:10:38 +0800

    Initial commit

---
+++ src/main/java/com/praveen/spring_ai_eval/SpringAiEvalApplication.java
@@ -0,0 +1,13 @@
+package com.praveen.spring_ai_eval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiEvalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiEvalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/praveen/spring_ai_eval/SpringAiEvalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveen.spring_ai_eval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiEvalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
